75816d65af5380382e7854720f919ebdfdc1ba61
Medina Pilavdzic <medina.pilavdzic@gmail.com>
2021-03-26 08:54:53 +0100

    Little changes in console.log list order

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -4,10 +4,13 @@ let two = new Set([6, 4, 3, 2]);
 let unionSet = new Set([...one, ...two]);
 let intersectionSet = new Set([...one].filter((i) => two.has(i)));
 let differenceSet = new Set([...one].filter((i) => !two.has(i)));
+
+console.log(one);
+console.log(two);
 console.log(unionSet);
 console.log(intersectionSet);
 console.log(differenceSet);
-console.log(one, two);
+
 
 // Only change code above this line
 module.exports = { unionSet, intersectionSet, differenceSet };
